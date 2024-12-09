b48e77074633848d754ad14863f77efcc9c38089
Aizaz-Ahmed786 <aizazahmed046@gmail.com>
2024-12-09 12:58:09 +0500

    class 1 add hwa h or class2 bhi

---
+++ kaleem.dart
@@ -0,0 +1,20 @@
+void main() {
+  print("the vtc naudero");
+  print("students informition");
+  String name = "name";
+  String age = "age";
+  String gender = "gender";
+  var email = "email";
+  var contact = "contact";
+  print(name);
+  print(age);
+  print(gender);
+  print(email);
+  print(contact);
+  bool result = 12 < 8;
+  int a = 12;
+  int b = 50;
+  int sum = a + b;
+  print(result);
+  print(sum);
+}
---
+++ test.dart
@@ -0,0 +1,35 @@
+void main() {
+  // Equality & Relational Operator
+
+  // EQUAL SYMBOL IS  -->   ==
+  num num1 = 34;
+  num num2 = 34;
+  print(num1 == num2); //true
+
+  // NOT EQUAL TO -->    !=
+  num num3 = 34;
+  num num4 = 4;
+  print(num3 != num4); //true
+
+  // Grater than -->  >
+
+  num num5 = 34;
+  num num6 = 4;
+  print(num3 > num4); //true
+
+  // Lass than -->  <
+
+  num num7 = 34;
+  num num8 = 4;
+  print(num7 < num8); //true
+
+// Greater than or equal to
+  num num9 = 34;
+  num num10 = 4;
+  print(num9 >= num10);
+
+  // Less than or equal to
+  num num11 = 34;
+  num num12 = 4;
+  print(num11 <= num12);
+}
